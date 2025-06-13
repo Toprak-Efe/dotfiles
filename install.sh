5163c2527cd77f4252d13843465f452a31aa3179
Toprak <efe.akkilic@ozu.edu.tr>
2025-06-13 22:53:26 +0200

    Updated on Fri Jun 13 10:53:26 PM CEST 2025.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ BIN_DIR="${XDG_CACHE_HOME:-$HOME/.local/bin}"
 SYM_DIR="/usr/bin"
 
 for script in $(ls "$PWD/scripts"); do
-    chmod +rx "$PWD/scripts/$script"
+    sudo chmod +rx "$PWD/scripts/$script"
     cp "$PWD/scripts/$script" "$BIN_DIR/"
     sudo ln -fs "$BIN_DIR/$script" "$SYM_DIR/$script"
 done
@@ -13,4 +13,9 @@ done
 for config in $(ls "$PWD/config"); do
     cp -r "$PWD/config/$config" "$CFG_DIR/$config"
 done
+
+sudo ln -fs "$PWD/update.sh" "$BIN_DIR/update.sh"
+sudo cp "$PWD/service/dotfiles.timer" "/etc/systemd/system/"
+sudo sh -c 'echo "$(cat $PWD/service/dotfiles.service)$PWD" > /etc/systemd/system/dotfiles.service'
+sudo systemctl enable --now dotfiles.timer
 
--- update.sh
+++ update.sh
@@ -3,14 +3,7 @@ BIN_DIR="${XDG_CACHE_HOME:-$HOME/.local/bin}"
 SYM_DIR="/usr/bin"
 
 for config in $(ls "$PWD/config"); do
-    read -p "Updating folders for $config, are you sure? " -n 1 -r
-    echo    
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        cp -r "$PWD/config/$config" "$CFG_DIR/$config"
-        echo "Copied $config."
-        # do dangerous stuff
-    fi
+    cp -r "$PWD/config/$config" "$CFG_DIR/$config"
 done
 
 git add . && git commit -m "Updated on $(date)." && git push origin main
